f6587d256f3570d92fc4e0a353c708fd50a21f39
stas <stas@vlad-2.Dlink>
2019-01-07 18:01:38 +0300

    12121221

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -128,7 +128,7 @@ export async function setContext(app, context) {
 
       payload: context.payload,
       error: context.error,
-      base: '/',
+      base: '/poloski-sample/',
       env: {}
     }
     // Only set once
